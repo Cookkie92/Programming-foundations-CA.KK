d41bf3d2c5c8d23f9d1977325b55a293fdfd68a1
Cookkie92 <74307133+Cookkie92@users.noreply.github.com>
2021-06-09 14:40:49 +0200

    Update script.js

---
+++ programming-foundations-ca-master/js/script.js
@@ -0,0 +1,65 @@
+//Javascript CA1
+
+
+//Question 1
+
+
+var outOfStock = true;
+
+if(outOfStock === true){
+    //console.log("Out of stock");
+}
+else{
+    //console.log("In stock");
+}
+
+
+//Question 2
+
+
+for (var i = 15; i <=25; i++){
+    if(i === 17 || i === 20 ){ 
+        //console.log(i);   
+    }
+}
+
+
+
+
+
+
+//Question 3
+
+var games = [
+	{
+		title: "Grand Theft Auto",
+		rating: 4.48,
+	},
+	{
+		title: "Portal 2",
+		rating: 3.5,
+	},
+	{
+		title: "Team Fortress",
+		rating: null,
+	},
+	{
+		title: "The Witcher",
+		rating: 3.0,
+	},
+	{
+		title: "The Elder Scrolls",
+		rating: 2.9,
+	},
+];
+
+
+
+
+
+
+
+
+
+
+//Question 4
